061d0e4697be65174da12b3cddd5dcf894f8720e
Bilal Karim <beekay-@users.noreply.github.com>
2015-07-08 00:50:34 -0600

    Updated jQuery version

--- js/countdown.js
+++ js/countdown.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
     $(".clock").countdown("2015/07/20", function (event) {
-        $(this).text(event.strftime('%D days %H:%M:%S'));
+        $(this).text(
+            event.strftime('%D days %H:%M:%S')
+        );
     });
 });
