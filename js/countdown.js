9cdcd2a01e5a6e288076cbdee6961466841b3d8c
Bilal Karim <beekay-@users.noreply.github.com>
2015-07-08 00:47:04 -0600

    Replaced jQuery countdown plugin

--- js/countdown.js
+++ js/countdown.js
@@ -1,9 +1,5 @@
 $(document).ready(function () {
-    var date = new Date(2015, 6, 20);
-    var now = new Date();
-    var diff = (date.getTime() / 1000) - (now.getTime() / 1000);
-    var clock = $('.clock').FlipClock(diff, {
-        clockFace: 'DailyCounter',
-        countdown: true
+    $(".clock").countdown("2015/07/20", function (event) {
+        $(this).text(event.strftime('%D days %H:%M:%S'));
     });
 });
